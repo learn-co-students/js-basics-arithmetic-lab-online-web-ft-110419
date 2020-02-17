bdd926f42d722bf504c6b694848c827468b11b16
Brandon Weaver <a7xncob@gmail.com>
2020-02-17 02:51:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here
+const var1 = 2;
+const var2 = 31;
+const multiply = var1 * var2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = 15 % 11;
+const max = Math.max(2, 5, 10, 20);

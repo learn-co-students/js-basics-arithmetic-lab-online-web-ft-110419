e89a3e19840eb5215f04bcf3f907963323f15516
jessesbyers <jesse.s.byers@gmail.com>
2020-02-17 14:13:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here
+const var1 = 15.5;
+const var2 = 4;
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 34 % 6;
+
+let max = Math.max(2, 5, 6, 20, 18);
